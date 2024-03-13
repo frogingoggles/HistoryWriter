653e86762ef83c1de0835c0ab1bd18d1c14b4ae9
Sam Sawaya <samsawaya@gmail.com>
2024-03-13 17:40:57 -0500

    Initial Commit

---
+++ HistoryWriter/HistoryWriterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryWriterApp.swift
+//  HistoryWriter
+//
+//  Created by Sam Sawaya on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HistoryWriterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
